a66f96ee82f0c56345baae66fec28c656f88b370
MonikaVen <monika.venckauskaite@gmail.com>
2024-06-16 19:59:09 +0300

    Agentic LLM test

---
+++ test_agent.py
@@ -0,0 +1,85 @@
+from helper import get_openai_api_key
+
+OPENAI_API_KEY = get_openai_api_key()
+
+print(OPENAI_API_KEY)
+
+import nest_asyncio
+
+nest_asyncio.apply()
+
+from llama_index.core import SimpleDirectoryReader
+
+# load documents
+documents = SimpleDirectoryReader(input_files=["kvietimas.pdf"]).load_data()
+
+from llama_index.core.node_parser import SentenceSplitter
+
+splitter = SentenceSplitter(chunk_size=1024)
+nodes = splitter.get_nodes_from_documents(documents)
+
+from llama_index.core import Settings
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+
+Settings.llm = OpenAI(model="gpt-3.5-turbo")
+Settings.embed_model = OpenAIEmbedding(model="text-embedding-ada-002")
+
+from llama_index.core import SummaryIndex, VectorStoreIndex
+
+summary_index = SummaryIndex(nodes)
+vector_index = VectorStoreIndex(nodes)
+
+summary_query_engine = summary_index.as_query_engine(
+    response_mode="tree_summarize",
+    use_async=True,
+)
+vector_query_engine = vector_index.as_query_engine()
+
+from llama_index.core.tools import QueryEngineTool
+
+
+summary_tool = QueryEngineTool.from_defaults(
+    query_engine=summary_query_engine,
+    description=(
+        "Useful for summarization questions related to doc"
+    ),
+)
+
+vector_tool = QueryEngineTool.from_defaults(
+    query_engine=vector_query_engine,
+    description=(
+        "Useful for retrieving specific context from the doc."
+    ),
+)
+
+#Define router query engine
+from llama_index.core.query_engine.router_query_engine import RouterQueryEngine
+from llama_index.core.selectors import LLMSingleSelector
+
+
+query_engine = RouterQueryEngine(
+    selector=LLMSingleSelector.from_defaults(),
+    query_engine_tools=[
+        summary_tool,
+        vector_tool,
+    ],
+    verbose=True
+)
+
+response = query_engine.query("Parašyk dokumento santrauką.")
+print(str(response))
+
+from py_trans import PyTranslator
+
+tr = PyTranslator()
+
+resp_str = str(response["translation"])
+print(tr.google(resp_str, "lt"))
+
+
+
+response = query_engine.query(
+    "Kokia suma skiriama projekto finansavimui?"
+)
+print(str(response))
